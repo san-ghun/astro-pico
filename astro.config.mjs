9ac538556e0392f305d8de89a207d29a8f88b4a5
Sanghun <po4865@gmail.com>
2024-05-16 15:41:34 +0200

    feat: Add social links with icons
    - Add astro-icon integration.
    - Add .svg files for icon data.
    - Add icon to Social.astro component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()]
 });
