d2231ac155d6bbe6140d2342a5d9e252978d76eb
Sanghun <po4865@gmail.com>
2024-05-30 00:02:16 +0200

    config: Add Astro integration for MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()],
+  integrations: [react(), icon(), mdx()],
   image: {
     domains: ["astro.build"],
-    remotePatterns: [{ protocol: "https" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+      },
+    ],
   },
   site: "https://astro-pico.netlify.app",
 });
