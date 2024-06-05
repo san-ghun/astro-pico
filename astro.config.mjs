074f44edafc123b80ba9339b115ca7652868d1c2
Sanghun <po4865@gmail.com>
2024-06-05 22:02:21 +0200

    feat: Install & add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon(), mdx()],
+  site: "https://astro-pico.netlify.app",
+  integrations: [react(), icon(), mdx(), sitemap()],
   image: {
     domains: ["astro.build"],
     remotePatterns: [
@@ -14,5 +16,4 @@ export default defineConfig({
       },
     ],
   },
-  site: "https://astro-pico.netlify.app",
 });
