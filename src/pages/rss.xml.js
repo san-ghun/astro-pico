d5858a2df2a17f3090f5c68d79dd8af74df3796c
Sanghun <po4865@gmail.com>
2024-08-26 23:37:01 +0200

    fix: Correct Pico from PicoCSS to Pico CSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,7 +5,7 @@ export async function GET(context) {
   const posts = await getCollection("posts");
   return rss({
     title: "AstroPico",
-    description: "Astro styled with PicoCSS",
+    description: "Astro styled with Pico CSS",
     site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
